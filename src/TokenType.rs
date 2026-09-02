cc6de4cf8bef8a427d6742d6db8063d8a88b4779
PrestonScott05 <pslumpkins@gmail.com>
2026-09-02 13:36:17 -0500

    added tokentypes

---
+++ src/TokenType.rs
@@ -0,0 +1,16 @@
+enum TokenType {
+  LeftParen, RightParen, LeftBrace, RightBrace,
+  Comma, Dot, Minue, Plus, Semicolon, Slash, Star,
+
+  Bang, Bang_Equal,
+  Equal, EqualEqual,
+  Greater, GreaterEqual,
+  Less, LessEqual,
+
+  Identifier, String, Number,
+
+  And, Class, Else, False, Fun, For, If, Nil, Or,
+  Print, Return, Super, This, True, Var, While,
+
+  Eof
+}
